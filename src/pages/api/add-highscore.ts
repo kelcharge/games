636bf445435e9563affc8fcd422adbdecedaf8b6
Daniel De Camp <theonly2017@gmail.com>
2022-07-21 09:29:04 -0500

    Check referer

--- src/pages/api/add-highscore.ts
+++ src/pages/api/add-highscore.ts
@@ -1,9 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { env } from "process";
 
 import { prisma } from "../../db/client";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     const body = req.body;
+    const referer = req.headers.referer;
+
+    if (referer !== env.REFERER) {
+        res.statusCode = 200;
+        res.send("");
+        return;
+    }
 
     const highscore = await prisma.highScore.create({
         data: body
